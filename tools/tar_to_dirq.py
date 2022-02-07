7ef241ca77ab2693425c1b62d205d935d7cb2274
tvv28669 <connor.pettitt@stfc.ac.uk>
2022-02-07 14:45:16 +0000

    Simplify command line arguments

--- tools/tar_to_dirq.py
+++ tools/tar_to_dirq.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""Module for loading json messages from a tar archive into a dirq directory
+"""Module for loading json messages into a dirq directory
+
+This script can feed either a single message file or a tar
+file of messages into a queue.
 
 Author: Connor Pettitt
 Contact: connor.pettitt@stfc.ac.uk
@@ -15,7 +18,9 @@ from dirq import queue
 schema = {"body": "string", "signer": "string",
           "empaid": "string?", "error": "string?"}
 
-def main(messages_tar=None, path='./'):
+default_path = './queue/incoming'
+
+def main(messages_tar=None, path='./queue'):
     """ DOC
     messages_tar: filename
         Existing filename to tar archive containing files with message bodies.
@@ -25,13 +30,16 @@ def main(messages_tar=None, path='./'):
     """
     message_strings = []
 
-    try:
-        with tarfile.open(name=messages_tar, mode='r', fileobj=None, bufsize=10240) as tar:
-            for file in tar:
-                message_strings.append(tar.extractfile(file).read())
-    except:
-        print(f'Not a valid tar archive: {messages_tar}')
-        exit(1)
+    if 'tar' in messages_tar:
+        try:
+            with tarfile.open(name=messages_tar, mode='r', fileobj=None, bufsize=10240) as tar:
+                for file in tar:
+                    message_strings.append(tar.extractfile(file).read())
+        except:
+            print(f'Not a valid tar archive: {messages_tar}')
+            exit(1)
+    else: # Try to load as text file
+        message_strings = [open(messages_tar, 'r').read()]
 
     message_bodies = []
 
@@ -72,24 +80,32 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Get input and output pathnames.')
 
-    parser.add_argument('-m/--messages', dest='messages_tar', required=True, 
-                        help='Supply a filename for input messages.')
-    parser.add_argument('-p/--path', dest='path', default='./', 
-                        help='Supply a path for Queue output (default: ./).')
+    parser.add_argument('messages_tar', default=None, help='Supply a filename for input messages')
+    parser.add_argument('-p/--path', dest='path', default='./queue/incoming',
+                        help=f'Supply a path for Queue output (default: {default_path}).',
+                        required=False)
 
     args = parser.parse_args()
 
     messages_tar = args.messages_tar
 
     if not os.path.exists(messages_tar):
-        print(f'File does not exist: {messages_tar}')
+        print(f'Tried to find file: {messages_tar} but it appears not to exist.')
+        print('Please use flag --messages FILENAME with either a text file or a tar archive.')
         exit(1)
 
     try:
         open(messages_tar, 'r')
     except:
-        print(f'Not a readable file: {messages_tar}')
+        print(f'Tried to read: {messages_tar} but failed. ')
+        print(f'Please check that path is a file and has the correct permissions.')
         exit(1)
+
+    if args.path == default_path:
+        print(f'Defaulting to path: {default_path} for output queue.')
+
+    if not os.path.exists(args.path):
+        os.makedirs(args.path)
 
     path = args.path
     messages_tar = args.messages_tar
